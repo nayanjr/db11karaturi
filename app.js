ffd94b5b217100e755c9f119e151b7e73674ee52
Kamal Nayan Karaturi <89647951+nayanjr@users.noreply.github.com>
2021-11-03 18:33:24 -0500

    modified

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var teaRouter = require('./routes/tea');
-var addmodsRouter = require('./routes/addmods');
-var selectorRouter = require('./routes/selector');
+//var addmodsRouter = require('./routes/addmods');
+//var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,8 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tea', teaRouter);
-app.use('/addmods', addmodsRouter);
-app.use('/selector', selectorRouter);
+//app.use('/addmods', addmodsRouter);
+//app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
